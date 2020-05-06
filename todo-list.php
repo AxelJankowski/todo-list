33cd7a47ca3f08cd1624f6f06b313139c0581515
Axel <axeljankowskii@gmail.com>
2020-05-06 17:24:17 +0200

    Starting work.

---
+++ todo-list.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @since 1.0.0
+ * @package ToDo List
+ * 
+ * @wordpress-plugin
+ * Plugin Name: ToDo List
+ * Description: Simple ToDo list plugin created as a recruitment task for MPC.
+ * Version:     1.0.0
+ * Author:      Axel Jankowski
+ * Author URI:  https://axeljankowski.github.io
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Enqueue frontend scripts.
+ */
+function frontend_scripts() {
+	wp_enqueue_script(
+	'wds-wwe-frontend-js',
+	plugins_url( 'assets/js/frontend.js', __FILE__ ),
+	[ 'jquery' ],
+	'11272018'
+	);
+}
+add_action( 'wp_enqueue_scripts', 'frontend_scripts' );
+
+/**
+ * Enqueue admin scripts.
+ */
+function admin_scripts() {
+	wp_enqueue_script(
+	'wds-wwe-admin-js',
+	plugins_url( 'assets/js/admin.js', __FILE__ ),
+	[ 'jquery' ],
+	'11272018'
+	);
+}
+add_action( 'admin_enqueue_scripts', 'admin_scripts' );
